2116d0308ef7ff905cb6dbe8cf0390fd2fd33b38
mitzengithub <root@DESKTOP-SQD2CVO.localdomain>
2024-02-21 20:37:08 +1300

    tested integration and adding more component

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,47 @@ mod cmd;
 mod netdiscovery;
 
 fn main() {
-
+    
     use clap::Parser;
     let cli = cmd::Cli::parse();
-
+    
     println!("name: {:?}", cli.target.as_deref());
     
     // // You can check the value provided by positional arguments, or option arguments
     if let Some(hostname) = cli.target.as_deref() {
         println!("Value for name: {hostname}");
         
-    let mut provider = netdiscovery::Provider::new(Vec::new());
+        let mut provider = netdiscovery::Provider::new(Vec::new());
         
+        let ping = netdiscovery::PingProvier{};
+        provider.add_task(&ping);
+        provider.execute();    
         
     }
     
     // // if let Some(config_path) = cli.config.as_deref() {
-    // //     println!("Value for config: {}", config_path.display());
-    // // }
-    
-    // // You can see how many times a particular flag or argument occurred
-    // // Note, only flags can have multiple occurrences
-    // match cli.debug {
-    //     0 => println!("Debug mode is off"),
-    //     1 => println!("Debug mode is kind of on"),
-    //     2 => println!("Debug mode is on"),
-    //     _ => println!("Don't be crazy"),
-    // }
-    
-    // // You can check for the existence of subcommands, and if found use their
-    // // matches just as you would the top level cmd
-    // match &cli.command {
-    //     Some(cmd::Commands::Test { list }) => {
-    //         if *list {
-    //             println!("Printing testing lists...");
-    //         } else {
-    //             println!("Not printing testing lists...");
-    //         }
-    //     }
-    //     None => {}
-    // }    
-}
+        // //     println!("Value for config: {}", config_path.display());
+        // // }
+        
+        // // You can see how many times a particular flag or argument occurred
+        // // Note, only flags can have multiple occurrences
+        // match cli.debug {
+            //     0 => println!("Debug mode is off"),
+            //     1 => println!("Debug mode is kind of on"),
+            //     2 => println!("Debug mode is on"),
+            //     _ => println!("Don't be crazy"),
+            // }
+            
+            // // You can check for the existence of subcommands, and if found use their
+            // // matches just as you would the top level cmd
+            // match &cli.command {
+                //     Some(cmd::Commands::Test { list }) => {
+                    //         if *list {
+                        //             println!("Printing testing lists...");
+                        //         } else {
+                            //             println!("Not printing testing lists...");
+                            //         }
+                            //     }
+                            //     None => {}
+                            // }    
+                        }
--- src/netdiscovery.rs
+++ src/netdiscovery.rs
@@ -1,4 +1,8 @@
 
+pub trait NetworkTask {
+    fn execute(&self, s: String);
+}
+
 pub struct Provider<'a> {
     tasklist: Vec<&'a dyn NetworkTask>,
 }
@@ -18,8 +22,14 @@ impl<'a> Provider<'a> {
     pub fn add_task(&mut self, task: &'a dyn NetworkTask) {
         self.tasklist.push(task); // Now modifying taskList is allowed
     }
+    
+    pub fn execute(&self)
+    {
+        for n in self.tasklist.iter() {
+            n.execute(String::from("test"));
+        }
+    }
 }
-
 
 // pub struct Provider {
 //     mut taskList: Vec<dyn NetworkTask> 
@@ -30,18 +40,34 @@ impl<'a> Provider<'a> {
 // }
  
 pub struct PingProvier { 
-
-}
-
-pub trait NetworkTask {
-    fn execute(&self, s: String);
 }
 
 impl NetworkTask for PingProvier {
     fn execute(&self, s: String) {
-        println!("executing ping provider {}", s);
+        println!("executing ping provider from trait: {}", s);
     }
 } 
+
+pub struct SqlServerDatabaseConnection { 
+}
+
+impl NetworkTask for SqlServerDatabaseConnection {
+    fn execute(&self, s: String) {
+        println!("executing database connection provider: {}", s);
+    }
+}
+
+pub struct TraceRouter { 
+
+}
+
+impl NetworkTask for TraceRouter {
+    fn execute(&self, s: String) {       
+        println!("executing traceroute provider: {}", s);
+    }
+}
+
+
 
 // impl<'a> Provider<'a> {
 //     pub fn add_task(&self, task: &dyn NetworkTask) {
